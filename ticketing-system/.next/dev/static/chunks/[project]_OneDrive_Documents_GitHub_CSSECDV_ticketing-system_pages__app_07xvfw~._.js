9fdededef5fc5132f8b42122d74e943de34c8a79
Jorenie <118247887+JtotheE@users.noreply.github.com>
2026-03-31 18:09:00 +0800

    Rearranged Files
    Rearranged the files out of the ticketing-system folder

---
+++ ticketing-system/.next/dev/static/chunks/[project]_OneDrive_Documents_GitHub_CSSECDV_ticketing-system_pages__app_07xvfw~._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/08-n_next_dist_compiled_0_.6t.v._.js","static/chunks/08-n_next_dist_shared_lib_0_9upr0._.js","static/chunks/08-n_next_dist_client_01452us._.js","static/chunks/08-n_next_dist_0cz77t6._.js","static/chunks/05bx_next_app_0g1tl3j.js","static/chunks/[next]_entry_page-loader_ts_10y_-ar._.js","static/chunks/08-n_react-dom_121_5~j._.js","static/chunks/08-n_0sx-igs._.js","static/chunks/[root-of-the-server]__01q9e98._.js"],
+    source: "entry"
+});
